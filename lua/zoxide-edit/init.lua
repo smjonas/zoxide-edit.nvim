84a1e4667b1ce8bf44595f74cc588d398addefcd
smjonas <jonas.strittmatter@gmx.de>
2023-03-06 11:56:49 +0100

    fix: correct function arguments to on_stdout

--- lua/zoxide-edit/init.lua
+++ lua/zoxide-edit/init.lua
@@ -8,7 +8,7 @@ local exec = function(opts)
   local entered_path = opts.args
   local cmd = { "zoxide", "query", "--", entered_path }
   vim.fn.jobstart(cmd, {
-    on_stdout = function(zoxide_result)
+    on_stdout = function(_, zoxide_result)
       vim.cmd("e " .. zoxide_result[1])
     end,
     on_stderr = function(_, data)
@@ -18,7 +18,7 @@ local exec = function(opts)
 end
 
 M.setup = function(user_config)
-  config = vim.tbl_deep_extend("force", default_config, user_config or {})
+  local config = vim.tbl_deep_extend("force", default_config, user_config or {})
   if config.abbreviate_e_cmd then
     vim.cmd(":cabbrev e <c-r>=(getcmdtype()==':' && getcmdpos()==1 ? 'E' : 'e')<CR>")
   end
